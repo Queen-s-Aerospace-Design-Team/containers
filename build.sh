ef8c2b0cfed4bc76e084653e156de55e8125b727
Anthony Botticchio <anthonyjb5228@gmail.com>
2026-01-04 16:43:42 -0500

    Changes FROM to osrf/ros-humble-desktop to include rviz by default. Edited

--- build.sh
+++ build.sh
@@ -10,27 +10,28 @@ general() {
     FOLDER=$1
     ACTION=${2:-local} # Action defaults to local builds
 
-    GIT_USERNAME="$(git config --get user.name)"
-    REGISTRY="ghcr.io"
-    ORG="queen-s-aerospace-design-team"
-    TAG="latest"
-    PLATFORMS=("linux/amd64" "linux/arm64")
-
-    if [[ "$ACTION" == "push" ]]; then
-        TARGET_PLATFORMS="$(IFS=,; echo "${PLATFORMS[*]}")"   # multi-arch for pushes
-    else
-        TARGET_PLATFORMS="$(docker version -f '{{.Server.Os}}/{{.Server.Arch}}')"  # host arch
-    fi
-
     [[ -z "$FOLDER" ]] && { echo "Usage: $0 <folder-with-Dockerfile> [push|local]"; exit 1; }
     [[ -f "$FOLDER/Dockerfile" ]] || { echo "Error: $FOLDER/Dockerfile not found"; exit 1; }
 
-    BASE="$(basename "$FOLDER")"
-    NAME="${BASE}"
-    REF="${REGISTRY}/${ORG}/${NAME}:${TAG}"
+    GIT_USERNAME="$(git config --get user.name)"
+    REGISTRY="ghcr.io"
+    ORG="queen-s-aerospace-design-team"
+    PLATFORMS=("linux/amd64" "linux/arm64")
+    NAME="$(basename "$FOLDER")"
 
-    # Cache the image on the remote as well (GitHub Registry)
-    CACHE_IMAGE="${REGISTRY}/${ORG}/${NAME}:buildcache"
+    CACHE_IMAGE="${REGISTRY}/${ORG}/${NAME}:buildcache" # Cache the image on the remote as well (GitHub Registry)
+    CACHE_FROM_ARGS=( --cache-from=type=registry,ref="${CACHE_IMAGE}" ) # By default, pull cache from remote registry
+    CACHE_TO_ARGS=( --cache-to=type=registry,ref="${CACHE_IMAGE}",mode=max ) # Upload to registry cache only when a GitHub Token is provided (otherwise you can't run 'docker buildx build ... --push')
+
+    if [[ "$ACTION" == "push" ]]; then
+        TARGET_PLATFORMS="$(IFS=,; echo "${PLATFORMS[*]}")"   # multi-arch for pushes
+        TAG="latest"
+    else
+        TARGET_PLATFORMS="$(docker version -f '{{.Server.Os}}/{{.Server.Arch}}')"  # host arch
+        TAG="local"
+    fi
+
+    REF="${REGISTRY}/${ORG}/${NAME}:${TAG}"
 }
 
 ensure_builder() {
@@ -54,19 +55,15 @@ ensure_builder() {
 maybe_login_to_registry() {
     # Login only if a token is provided
     if [[ -n "${GHCR_TOKEN:-}" ]]; then
-        echo "$GHCR_TOKEN" | docker login "$REGISTRY" -u "$GIT_USERNAME" --password-stdin >/dev/null
+        echo "$GHCR_TOKEN" | docker login "$REGISTRY" -u "$GIT_USERNAME" --password-stdin
     fi
 }
 
 build() {
     echo "Building ${REF} for $TARGET_PLATFORMS..."
     export BUILDX_NO_DEFAULT_ATTESTATIONS=1
-
-    # By default, pull cache from remote registry
-    CACHE_FROM_ARGS=( --cache-from=type=registry,ref="${CACHE_IMAGE}" )
     
     ensure_builder
-    maybe_login_to_registry
 
     if [[ "$ACTION" == "push" ]]; then
         # Pushing the image requires auth; fail fast if missing
@@ -75,8 +72,7 @@ build() {
             exit 1
         fi
 
-        # Upload to registry cache only when a GitHub Token is provided (otherwise you can't run 'docker buildx build ... --push')
-        CACHE_TO_ARGS=( --cache-to=type=registry,ref="${CACHE_IMAGE}",mode=max )
+        maybe_login_to_registry
 
         docker buildx build \
             --platform "$TARGET_PLATFORMS" \
@@ -98,3 +94,6 @@ build() {
 
 general "$@"
 build
+
+echo
+echo "Finished publishing image '${REF}'"
--- devcontainer-px4/entrypoint.sh
+++ devcontainer-px4/entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-MicroXRCEDDS udp4 -p 8888 >/dev/null 2>&1 &
-
 echo "==> Container ready"
 
 exec "$@"
